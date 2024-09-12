0d3c052e9a167797586391b971ff9b4970b293ea
akashdp3 <akashdeep.samantra@protonmail.com>
2024-09-12 09:07:17 +0530

    Create http module

---
+++ express-rust/src/conn/mod.rs
@@ -0,0 +1,7 @@
+mod method;
+mod request;
+mod response;
+
+pub use method::Method;
+pub use request::Request;
+pub use response::Response;
--- express-rust/src/utils/server.rs
+++ express-rust/src/utils/server.rs
@@ -8,24 +8,22 @@ pub fn start_server(port: u16) {
         match stream {
             Ok(_) => {
                 println!("Accpected new connection");
-            },
+            }
             Err(error) => {
                 eprintln!("Error on receiving request. Error: {error}");
             }
         }
     }
-    
 }
 
 fn create_listener(port: u16) -> TcpListener {
     let server_address = format!("localhost:{port}");
 
-
     match TcpListener::bind(server_address) {
         Ok(listener) => {
             println!("Listening on port {port}");
             listener
-        },
+        }
         Err(error) => {
             eprintln!("Error occured while trying to listen on {port}. Error: {error}");
             panic!("Could not start server on {port}");

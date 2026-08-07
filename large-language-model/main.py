b22d89305542bd62afd5a04761f2481808c62a78
akashdp3 <akashdeep.samantra@protonmail.com>
2026-08-07 16:33:56 +0530

    Chapter 2: Converting tokens to IDs

---
+++ large-language-model/main.py
@@ -0,0 +1,24 @@
+import re
+
+with open("the-verdict.txt", "r", encoding="utf-8") as f:
+    raw_text = f.read()
+
+print("Total number of character:", len(raw_text))
+print(raw_text[:99])
+
+# split the raw_text into tokens
+preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', raw_text)
+preprocessed = [item.strip() for item in preprocessed if item.strip()]
+print(len(preprocessed))
+print(preprocessed[:30])
+
+# creating vocabulary
+all_words = sorted(set(preprocessed))
+vocab_size = len(all_words)
+print(vocab_size)
+
+vocab = { token:index for index, token in enumerate(all_words) }
+for i, item in enumerate(vocab.items()):
+    print(item)
+    if i >= 50:
+        break
